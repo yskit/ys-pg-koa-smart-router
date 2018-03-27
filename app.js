6ef98b7c46dfa8bb3b4633709153a1f22144e1c7
shenyunjie <shenyunjie@u51.com>
2018-03-27 20:09:27 +0800

    [update] debug module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Router = require('smart-koa-router');
 const fs = require('fs');
 const path = require('path');
 const util = require('ys-utils');
+const debug = require('debug')('pg-koa-smart-router');
 
 module.exports = (app, configs) => {
   app.on('serverWillStart', () => {
@@ -10,6 +11,7 @@ module.exports = (app, configs) => {
       throw new Error(`router of '${routerPath}' must be exists`);
     }
     const exports = util.file.load(routerPath);
+    debug('load router file', routerPath);
     app.router = new Router(configs);
     exports(app, app.router);
     app.koa.use(app.router.routes());
